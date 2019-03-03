6fc2cd34ac6721181d50f2e54942bf661fe9c45d
Nazar Homeniuk <gomenyuknazar@gmail.com>
2019-03-03 19:31:00 +0200

    Modified navbar hiding behavior.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos < currentScrollPos) {
+  if (currentScrollPos > 20) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-60px";
