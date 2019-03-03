e9fbf065e3e278228d17022d556afad13e1c2dc7
Nazar Homeniuk <gomenyuknazar@gmail.com>
2019-03-03 19:08:42 +0200

    Added GoTop button. Added footer.

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,15 @@ window.onscroll = function() {
     document.getElementById("navbar").style.top = "-60px";
   }
   prevScrollpos = currentScrollPos;
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topBtn").style.display = "block";
+  } else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
+function goTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
